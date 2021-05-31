628065b2a370b64d8c9751fdcfd5bfdc0a6703eb
SalmaAhmed20 <64385957+SalmaAhmed20@users.noreply.github.com>
2021-05-31 07:54:17 +0200

    indexed

--- src/VFS/Indexed.java
+++ src/VFS/Indexed.java
@@ -3,6 +3,8 @@ package VFS;
 public class Indexed {
     private Directory root = new Directory ();
     private final int DiskSize = 30; //KB
-    private int FreeBlocks =DiskSize;
+    private String B = "000000000000000000000000000000"; //intial value
+    char [] Blocks = this.B.toCharArray();
+    private int FreeBlocks = DiskSize;
 
 }
