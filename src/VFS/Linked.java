cae105bcf2a3505bd06008a12e53ef76bd08e459
SalmaAhmed20 <64385957+SalmaAhmed20@users.noreply.github.com>
2021-05-31 09:03:29 +0200

    linked

--- src/VFS/Linked.java
+++ src/VFS/Linked.java
@@ -1,4 +1,9 @@
 package VFS;
 
 public class Linked {
+    private final Directory root = new Directory ();
+    private final int DiskSize = 30; //KB
+    private String B = "000000000000000000000000000000"; //intial value
+    char [] Blocks = this.B.toCharArray();
+    private int FreeBlocks = DiskSize;
 }
